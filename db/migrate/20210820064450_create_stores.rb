5a24d0a005ee6142ceab973de9f4d0d6735e279b
ft529y <yuto529reds@gmail.com>
2021-08-20 16:25:15 +0900

    storeマイグレーションファイル記述

---
+++ db/migrate/20210820064450_create_stores.rb
@@ -0,0 +1,9 @@
+class CreateStores < ActiveRecord::Migration[6.0]
+  def change
+    create_table :stores do |t|
+      t.string :store_name,         null: false
+      t.text :description,          null: false
+      t.timestamps
+    end
+  end
+end
