a7d7f6eb318820e624b8a581bfe4e5474bc92954
ft529y <yuto529reds@gmail.com>
2021-08-27 17:44:35 +0900

    store コントローラー編集

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,4 +1,27 @@
 class StoresController < ApplicationController
   def index
   end
+
+  def list
+    @stores = Store.order('created_at DESC')
+  end
+
+  def new
+    @store = Store.new
+  end
+
+  def create
+    @store = Store.new(store_params)
+    if @store.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+end
+
+private
+
+def store_params
+  params.require(:store).permit(:store_name, :description, :image).merge(user_id: current_user.id)
 end
