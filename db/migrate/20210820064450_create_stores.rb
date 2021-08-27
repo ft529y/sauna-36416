125faddf906bbaa6b14036efb3f063ca642cfe75
ft529y <yuto529reds@gmail.com>
2021-08-27 12:15:59 +0900

    stores　マイグレーションファイル削除

--- db/migrate/20210820064450_create_stores.rb
+++
@@ -1,9 +0,0 @@
-class CreateStores < ActiveRecord::Migration[6.0]
-  def change
-    create_table :stores do |t|
-      t.string :store_name,         null: false
-      t.text :description,          null: false
-      t.timestamps
-    end
-  end
-end
