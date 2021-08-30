0e5ffd021cb16dac4051e4289a67e38e81cfa589
ft529y <yuto529reds@gmail.com>
2021-08-30 17:23:52 +0900

    store 単体テストコード記述

--- spec/factories/stores.rb
+++ spec/factories/stores.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :store do
-    
+    store_name            {'サウナ'}
+    description           {'サウナはとても気持ちいいです。'}
+    association :user
+
+    after(:build) do |store|
+      store.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
