367e709f005aca37f0f3ba2bde9cbc3fc0d66ff4
ft529y <yuto529reds@gmail.com>
2021-09-01 15:16:58 +0900

    comment モデル　バリデーション編集

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,9 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :store
 
-  validates :text,      presence: true
+  validates :text,       presence: true
+  validates :evaluation, numericality: {
+    less_than_or_equal_to: 5,
+    greater_than_or_equal_to: 0.5, message: "を入力して下さい。"
+  }
 end
