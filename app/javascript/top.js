641466fac1a24c249e92facb88685ac473ca7910
ft529y <yuto529reds@gmail.com>
2021-09-03 15:47:00 +0900

    top画面 javascript実装

---
+++ app/javascript/top.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+  const dts = document.querySelectorAll('dt');
+  
+  dts.forEach(dt => {
+    dt.addEventListener('click', () => {
+      dt.parentNode.classList.toggle('appear');
+
+      dts.forEach(el => {
+        if (dt !== el) {
+          el.parentNode.classList.remove('appear');
+        }
+      });
+    });
+  });
+};
+
+
