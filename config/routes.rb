b6001cc5a0faef13456e217990a1381935533891
ft529y <yuto529reds@gmail.com>
2021-09-07 14:03:50 +0900

    ルーティング編集

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :stores, only: [:index, :new, :create, :show] do
     collection do
-      get 'list'
+      get 'list', 'check'
     end
     resources :comments, only: [:new, :create]
   end
