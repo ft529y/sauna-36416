8146b6c7d6c795bf366a9afd3c3047ea629627c7
ft529y <yuto529reds@gmail.com>
2021-09-16 00:40:22 +0900

    applicationコントローラー編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,5 +12,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :birthday])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :last_name, :first_name, :birthday])
   end
 end
