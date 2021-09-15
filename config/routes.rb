56bd8e4f088117cb83bcf79c798201edf233b507
ft529y <yuto529reds@gmail.com>
2021-09-16 00:38:40 +0900

    ルーティング編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "stores#index"
-  devise_for :users
+  devise_for :users, controllers:{  registrations:'users/registrations'  }
   resources :stores, only: [:index, :new, :create, :show] do
     collection do
       get 'list', 'check'
