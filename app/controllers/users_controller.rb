055839e93f8466800974a8cba349875d8a7d2efc
ft529y <yuto529reds@gmail.com>
2021-09-16 18:43:38 +0900

    usersコントローラー編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:show, :list, :unsubscribe, :withdrawal]
   before_action :set_params, only: [:show, :list, :unsubscribe, :withdrawal]
-  before_action :check_user, only: [:list]
+  before_action :check_user, only: [:list, :unsubscribe, :withdrawal]
 
   require 'date'
 
