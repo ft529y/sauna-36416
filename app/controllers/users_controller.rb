bb373f0e1020377edecbffc460fc1e7e654ba602
ft529y <yuto529reds@gmail.com>
2021-09-16 16:26:42 +0900

    usersコントローラー編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,42 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:show, :unsubscribe, :withdrawal]
+  before_action :authenticate_user!, only: [:show, :list, :unsubscribe, :withdrawal]
+  before_action :set_params, only: [:show, :list, :unsubscribe, :withdrawal]
+  before_action :check_user, only: [:show, :list]
+
   require 'date'
 
   def index
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def list
-    @user = User.find(params[:id])
     @comments = @user.comments.includes(:store).order('created_at DESC')
   end
 
   def unsubscribe
-    @user = User.find(params[:id])
   end
 
   def withdrawal
-    @user = User.find(params[:id])
     @user.update(is_deleted: true)
     date_time = DateTime.now
     deleted_email = @user.email + date_time.strftime('%Y%m%d%H%M')
     @user.update(email: deleted_email)
     reset_session
     flash[:notice] = '退会処理を実行いたしました'
+    redirect_to root_path
+  end
+end
+
+private
+
+def set_params
+  @user = User.find(params[:id])
+end
+
+def check_user
+  unless current_user.id == @user.id
     redirect_to root_path
   end
 end
