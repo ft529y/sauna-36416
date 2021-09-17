fd6d45bc2760fb2f26a0e117f3dba9b6043677a4
ft529y <yuto529reds@gmail.com>
2021-09-17 20:01:06 +0900

    storeバリデーション追加

--- app/models/store.rb
+++ app/models/store.rb
@@ -3,9 +3,20 @@ class Store < ApplicationRecord
   has_many :comments
   has_one_attached :image
 
+  # extend ActiveHash::Associations::ActiveRecordExtensions
+  # belongs_to :prefecture
+  # belongs_to :rouryu
+  # belongs_to :outside_bath
+  # belongs_to :sauna_temp
+  # belongs_to :water_temp
+
   with_options presence: true do
     validates :store_name, length: { maximum: 30 }
     validates :description, length: { maximum: 130 }
+    validates :address
+    VALID_PHONE_CODE_REGIX = /\A\d{10}$|^\d{11}\z/
+    validates :phone_number, format: { with: VALID_PHONE_CODE_REGIX }
+    validates :bathing_fee, numericality: { only_integer: true }
     validates :image
   end
 end
