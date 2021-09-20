579f66fd54921c5c72f99bed768801a07711866a
ft529y <yuto529reds@gmail.com>
2021-09-20 17:09:35 +0900

    rouryuモデル編集

--- app/models/rouryu.rb
+++ app/models/rouryu.rb
@@ -1,8 +1,12 @@
 class Rouryu < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
-    { id: 2, name: '有' },
-    { id: 3, name: '無' },
+    { id: 2, name: '有 (30分に1回)' },
+    { id: 3, name: '有 (60分に1回)' },
+    { id: 4, name: '有 (90分に1回)' },
+    { id: 5, name: '有 (120分に1回)' },
+    { id: 6, name: '有 (不定期実施)' },
+    { id: 7, name: '無' },
   ]
 
   include ActiveHash::Associations
