90e9e839242c0261d5e9bc0b3295f05b45642bb1
ft529y <yuto529reds@gmail.com>
2021-09-25 11:13:47 +0900

    FactoryBot Rubocop

--- spec/factories/stores.rb
+++ spec/factories/stores.rb
@@ -2,15 +2,15 @@ FactoryBot.define do
   factory :store do
     store_name            { 'サウナ' }
     description           { 'サウナはとても気持ちいいです。' }
-    sauna_temp            {'100'}
-    water_temp            {'15'}
-    rouryu_id             {2}
-    outside_bath_id       {2}
-    break_space_id        {2}
-    bathing_fee           {'1000'}
-    prefecture_id         {2}
-    address               {'東京都港区青山1-1-1サンプルビル100'}
-    phone_number          {'09012345678'}
+    sauna_temp            { '100' }
+    water_temp            { '15' }
+    rouryu_id             { 2 }
+    outside_bath_id       { 2 }
+    break_space_id        { 2 }
+    bathing_fee           { '1000' }
+    prefecture_id         { 2 }
+    address               { '東京都港区青山1-1-1サンプルビル100' }
+    phone_number          { '09012345678' }
     association :user
 
     after(:build) do |store|
