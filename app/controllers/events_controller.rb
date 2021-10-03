49883a0858326047075663a5d86748a5c7d439c2
ft529y <yuto529reds@gmail.com>
2021-10-03 18:18:52 +0900

    eventsコントローラー編集

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,21 @@
 class EventsController < ApplicationController
-  def index
+  def show
+    @events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_parameter)
+    if @event.save?
+      redirect_to root_path
+    end
+  end
+end
+
+private
+
+def event_parameter
 end
