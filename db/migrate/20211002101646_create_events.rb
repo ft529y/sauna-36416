deabe8ca5040f25aa8a465e77e79b954bd99586b
ft529y <yuto529reds@gmail.com>
2021-10-07 17:18:51 +0900

    event マイグレーションファイル編集

--- db/migrate/20211002101646_create_events.rb
+++ db/migrate/20211002101646_create_events.rb
@@ -1,7 +1,7 @@
 class CreateEvents < ActiveRecord::Migration[6.0]
   def change
     create_table :events do |t|
-      t.integer :execution_id
+      t.integer :execution_id,    null: false
       t.string :place
       t.integer :fee
       t.datetime :start_time
